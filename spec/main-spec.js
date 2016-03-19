72b4cb223a5c1a83ca38f0c3608b2d7c9ff15f1d
steelbrain <de.steelbrain@gmail.com>
2016-03-19 04:55:30 -0700

    :white_check_mark: Add specifications for provider

--- spec/main-spec.js
+++
@@ -1,3 +0,0 @@
-'use babel'
-
-console.log('hello world')
---
+++ spec/provider-spec.js
@@ -0,0 +1,52 @@
+'use babel'
+
+/* @flow */
+
+import Provider from '../lib/provider'
+
+describe('Provider', function() {
+  let provider
+
+  beforeEach(function() {
+    provider = new Provider()
+  })
+  afterEach(function() {
+    provider.dispose()
+  })
+
+  it('has a working way of setting, removing and emptying titles', function() {
+    expect(provider.texts.size).toBe(0)
+    provider.add('Hey')
+    expect(provider.texts.size).toBe(1)
+    const _ = Array.from(provider.texts)[0]
+    expect(_.title).toBe('Hey')
+
+    provider.remove('Hey')
+    expect(provider.texts.size).toBe(0)
+    provider.add('Wow')
+    expect(provider.texts.size).toBe(1)
+    provider.clear()
+    expect(provider.texts.size).toBe(0)
+  })
+
+  it('emits events properly', function() {
+    const change = jasmine.createSpy('change')
+    const dispose = jasmine.createSpy('dispose')
+
+    provider.onDidChange(change)
+    provider.onDidDestroy(dispose)
+
+    provider.add('Hey')
+    provider.remove('Hey')
+    provider.clear()
+    provider.add('Hey')
+    provider.clear()
+
+    provider.dispose()
+
+    expect(change).toHaveBeenCalled()
+    expect(dispose).toHaveBeenCalled()
+    expect(change.calls.length).toBe(4)
+    expect(dispose.calls.length).toBe(1)
+  })
+})
