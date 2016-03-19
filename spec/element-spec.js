588f64a9543eba527d810e7e01f4c54bd9c8ce8c
steelbrain <de.steelbrain@gmail.com>
2016-03-19 05:02:02 -0700

    :white_check_mark: Add specs for element

---
+++ spec/element-spec.js
@@ -0,0 +1,27 @@
+'use babel'
+
+import Element from '../lib/element'
+
+describe('Element', function() {
+  let element
+
+  beforeEach(function() {
+    element = new Element()
+    spyOn(element, 'setTooltip').andCallThrough()
+    spyOn(element, 'setBusy').andCallThrough()
+  })
+  afterEach(function() {
+    element.dispose()
+  })
+
+  it('sets a title properly', function() {
+    element.update(['Hello'])
+    expect(element.setBusy).toHaveBeenCalledWith(true)
+    expect(element.setTooltip).toHaveBeenCalledWith('Hello')
+  })
+  it('escapes the given texts', function() {
+    element.update(['<div>'])
+    expect(element.setBusy).toHaveBeenCalledWith(true)
+    expect(element.setTooltip).toHaveBeenCalledWith('&lt;div&gt;')
+  })
+})
