edee4ebad620b9e2d6723f4cfa2e2ddf9d84a446
lloiser <lukasberanek@gmx.net>
2018-12-29 17:36:52 +0100

    update dependencies

--- spec/element-spec.js
+++ spec/element-spec.js
@@ -1,42 +1,50 @@
 /* @flow */
 
-import Element from '../lib/element'
+import Element from "../lib/element";
 
-describe('Element', function() {
-  let element
+describe("Element", function() {
+  let element;
 
   beforeEach(function() {
-    element = new Element()
-    spyOn(element, 'setTooltip').andCallThrough()
-    spyOn(element, 'setBusy').andCallThrough()
-  })
+    element = new Element();
+    spyOn(element, "setTooltip").andCallThrough();
+    spyOn(element, "setBusy").andCallThrough();
+  });
   afterEach(function() {
-    element.dispose()
-  })
+    element.dispose();
+  });
 
-  it('sets a title properly', function() {
-    element.update(['Hello'], [])
-    expect(element.setBusy).toHaveBeenCalledWith(true)
-    expect(element.setTooltip).toHaveBeenCalledWith('<strong>Current:</strong><br>Hello')
-  })
-  it('escapes the given texts', function() {
-    element.update(['<div>'], [])
-    expect(element.setBusy).toHaveBeenCalledWith(true)
-    expect(element.setTooltip).toHaveBeenCalledWith('<strong>Current:</strong><br>&lt;div&gt;')
-  })
-  it('shows idle message when nothing is provided', function() {
-    element.update([], [])
-    expect(element.setBusy).toHaveBeenCalledWith(false)
-    expect(element.setTooltip).toHaveBeenCalledWith('Idle')
-  })
-  it('shows only history when current is not present', function() {
-    element.update([], [{ title: 'Yo', duration: '1m' }])
-    expect(element.setBusy).toHaveBeenCalledWith(false)
-    expect(element.setTooltip).toHaveBeenCalledWith('<strong>History:</strong><br>Yo (1m)')
-  })
-  it('shows both history and current when both are present', function() {
-    element.update(['Hey'], [{ title: 'Yo', duration: '1m' }])
-    expect(element.setBusy).toHaveBeenCalledWith(true)
-    expect(element.setTooltip).toHaveBeenCalledWith('<strong>History:</strong><br>Yo (1m)<br><strong>Current:</strong><br>Hey')
-  })
-})
+  it("sets a title properly", function() {
+    element.update(["Hello"], []);
+    expect(element.setBusy).toHaveBeenCalledWith(true);
+    expect(element.setTooltip).toHaveBeenCalledWith(
+      "<strong>Current:</strong><br>Hello"
+    );
+  });
+  it("escapes the given texts", function() {
+    element.update(["<div>"], []);
+    expect(element.setBusy).toHaveBeenCalledWith(true);
+    expect(element.setTooltip).toHaveBeenCalledWith(
+      "<strong>Current:</strong><br>&lt;div&gt;"
+    );
+  });
+  it("shows idle message when nothing is provided", function() {
+    element.update([], []);
+    expect(element.setBusy).toHaveBeenCalledWith(false);
+    expect(element.setTooltip).toHaveBeenCalledWith("Idle");
+  });
+  it("shows only history when current is not present", function() {
+    element.update([], [{ title: "Yo", duration: "1m" }]);
+    expect(element.setBusy).toHaveBeenCalledWith(false);
+    expect(element.setTooltip).toHaveBeenCalledWith(
+      "<strong>History:</strong><br>Yo (1m)"
+    );
+  });
+  it("shows both history and current when both are present", function() {
+    element.update(["Hey"], [{ title: "Yo", duration: "1m" }]);
+    expect(element.setBusy).toHaveBeenCalledWith(true);
+    expect(element.setTooltip).toHaveBeenCalledWith(
+      "<strong>History:</strong><br>Yo (1m)<br><strong>Current:</strong><br>Hey"
+    );
+  });
+});
--- spec/registry-spec.js
+++ spec/registry-spec.js
@@ -1,100 +1,106 @@
 /* @flow */
 
-import { it, wait } from 'jasmine-fix'
-import Registry from '../lib/registry'
+import { it, wait } from "jasmine-fix";
+import Registry from "../lib/registry";
 
-describe('Registry', function() {
-  let registry
+describe("Registry", function() {
+  let registry;
 
   beforeEach(function() {
-    registry = new Registry()
-  })
+    registry = new Registry();
+  });
   afterEach(function() {
-    registry.dispose()
-  })
-  function validateOldTiles(oldTitles: Array<{ title: string, duration: string }>, titles: Array<string>) {
-    expect(oldTitles.length).toBe(titles.length)
+    registry.dispose();
+  });
+  function validateOldTiles(
+    oldTitles: Array<{ title: string, duration: string }>,
+    titles: Array<string>
+  ) {
+    expect(oldTitles.length).toBe(titles.length);
 
     titles.forEach(function(title, index) {
-      expect(oldTitles[index].title).toBe(title)
-      expect(oldTitles[index].duration === '1ms' || oldTitles[index].duration === '0ms').toBe(true)
-    })
+      expect(oldTitles[index].title).toBe(title);
+      expect(
+        oldTitles[index].duration === "1ms" ||
+          oldTitles[index].duration === "0ms"
+      ).toBe(true);
+    });
   }
 
-  describe('handling of providers', function() {
-    it('registers providers properly and clears them out when they die', function() {
-      const provider = registry.create()
-      expect(registry.providers.has(provider)).toBe(true)
-      provider.dispose()
-      expect(registry.providers.has(provider)).toBe(false)
-    })
-    it('emits update event properly', function() {
-      const provider = registry.create()
-      const update = jasmine.createSpy('update')
-      registry.onDidUpdate(update)
-      expect(update).not.toHaveBeenCalled()
-      provider.add('Hey')
-      provider.remove('Hey')
-      provider.clear()
-      expect(update).toHaveBeenCalled()
-      expect(update.calls.length).toBe(2)
-    })
-    it('adds and returns sorted titles', async function() {
-      const provider = registry.create()
-      provider.add('Hey')
-      await wait(1)
-      provider.add('Wow')
-      await wait(1)
-      provider.add('Hello')
-      expect(registry.getTilesActive()).toEqual(['Hello', 'Wow', 'Hey'])
-    })
-    it('adds removed ones to history', async function() {
-      const provider = registry.create()
-      provider.add('Boy')
-      await wait(1)
-      provider.add('Hey')
-      expect(registry.getTilesActive()).toEqual(['Hey', 'Boy'])
-      expect(registry.getTilesOld()).toEqual([])
+  describe("handling of providers", function() {
+    it("registers providers properly and clears them out when they die", function() {
+      const provider = registry.create();
+      expect(registry.providers.has(provider)).toBe(true);
+      provider.dispose();
+      expect(registry.providers.has(provider)).toBe(false);
+    });
+    it("emits update event properly", function() {
+      const provider = registry.create();
+      const update = jasmine.createSpy("update");
+      registry.onDidUpdate(update);
+      expect(update).not.toHaveBeenCalled();
+      provider.add("Hey");
+      provider.remove("Hey");
+      provider.clear();
+      expect(update).toHaveBeenCalled();
+      expect(update.calls.length).toBe(2);
+    });
+    it("adds and returns sorted titles", async function() {
+      const provider = registry.create();
+      provider.add("Hey");
+      await wait(1);
+      provider.add("Wow");
+      await wait(1);
+      provider.add("Hello");
+      expect(registry.getTilesActive()).toEqual(["Hello", "Wow", "Hey"]);
+    });
+    it("adds removed ones to history", async function() {
+      const provider = registry.create();
+      provider.add("Boy");
+      await wait(1);
+      provider.add("Hey");
+      expect(registry.getTilesActive()).toEqual(["Hey", "Boy"]);
+      expect(registry.getTilesOld()).toEqual([]);
 
-      provider.remove('Hey')
-      expect(registry.getTilesActive()).toEqual(['Boy'])
-      validateOldTiles(registry.getTilesOld(), ['Hey'])
-    })
-    it('adds cleared ones to history', function() {
-      const provider = registry.create()
-      provider.add('Hello')
-      provider.add('World')
+      provider.remove("Hey");
+      expect(registry.getTilesActive()).toEqual(["Boy"]);
+      validateOldTiles(registry.getTilesOld(), ["Hey"]);
+    });
+    it("adds cleared ones to history", function() {
+      const provider = registry.create();
+      provider.add("Hello");
+      provider.add("World");
 
-      expect(registry.getTilesActive()).toEqual(['Hello', 'World'])
-      expect(registry.getTilesOld()).toEqual([])
+      expect(registry.getTilesActive()).toEqual(["Hello", "World"]);
+      expect(registry.getTilesOld()).toEqual([]);
 
-      provider.clear()
-      expect(registry.getTilesActive()).toEqual([])
-      validateOldTiles(registry.getTilesOld(), ['Hello', 'World'])
-    })
-  })
-  describe('getTilesOld', function() {
-    it('excludes active ones from history', function() {
-      const provider = registry.create()
-      provider.add('Yo CJ')
-      provider.add('Murica')
-      provider.remove('Yo CJ')
-      provider.remove('Murica')
-      provider.add('Yo CJ')
+      provider.clear();
+      expect(registry.getTilesActive()).toEqual([]);
+      validateOldTiles(registry.getTilesOld(), ["Hello", "World"]);
+    });
+  });
+  describe("getTilesOld", function() {
+    it("excludes active ones from history", function() {
+      const provider = registry.create();
+      provider.add("Yo CJ");
+      provider.add("Murica");
+      provider.remove("Yo CJ");
+      provider.remove("Murica");
+      provider.add("Yo CJ");
 
-      validateOldTiles(registry.getTilesOld(), ['Murica'])
-    })
-    it('excludes duplicates and only returns the last one', function() {
-      const provider = registry.create()
+      validateOldTiles(registry.getTilesOld(), ["Murica"]);
+    });
+    it("excludes duplicates and only returns the last one", function() {
+      const provider = registry.create();
 
-      provider.add('Some')
-      provider.add('Things')
-      provider.remove('Some')
-      provider.remove('Things')
-      provider.add('Some')
-      provider.remove('Some')
+      provider.add("Some");
+      provider.add("Things");
+      provider.remove("Some");
+      provider.remove("Things");
+      provider.add("Some");
+      provider.remove("Some");
 
-      validateOldTiles(registry.getTilesOld(), ['Things', 'Some'])
-    })
-  })
-})
+      validateOldTiles(registry.getTilesOld(), ["Things", "Some"]);
+    });
+  });
+});
